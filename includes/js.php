86a47ccf64d071cff2be6a3df950d3bfcd965cdd
Ridha Rahmi <ridha.rahmi13@gmail.com>
2021-05-27 16:56:18 +0100

    add files

---
+++ includes/js.php
@@ -0,0 +1,4 @@
+<script src="assets/js/drupal.js"></script>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
